6fade34c14fa5e70b134553b440cc8f8dca892e9
kabilesh2903 <kabil123arasan@gmail.com>
2025-10-13 00:33:29 +0800

    delete

--- module.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "instance" {
-  instance_type = var.typetera
-  key_name = var.keytera
-  ami = var.amitera
-  tags = {
-    Name=var.nametera
-  }
-}
--- variable.tf
+++
@@ -1,7 +0,0 @@
-variable "typetera" {}
-
-variable "keytera" {}
-
-variable "amitera" {}
-
-variable "nametera" {}
