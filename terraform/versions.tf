57eb1e1890413234d37fc5ab60043ef4acc16846
kabilesh2903 <kabil123arasan@gmail.com>
2025-10-14 01:11:19 +0800

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,10 +1,9 @@
 terraform {
   required_version = ">= 1.6.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.34"
+      version = "~> 6.18"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
